de12d81d47825a7aa10b1bd06d70b4f2fbf32331
Developer <44878271+advantage-development@users.noreply.github.com>
2019-11-12 18:32:03 -0600

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -132,7 +132,7 @@ public:
         assert(hashGenesisBlock == uint256("0x000002ab92c52760afa017db7c08fc825fcb19b585b891c8d2c9d9bd5bb527ea"));
         assert(genesis.hashMerkleRoot == uint256("0x586d223d971d097a6ead9743861a1bc8a518696669f0d8a4cc070e4b9ccae201"));
 
-        vSeeds.push_back(CDNSSeedData("167.71.102.31", "167.71.102.31"));
+        vSeeds.push_back(CDNSSeedData("165.22.231.39", "165.22.231.39"));
         vSeeds.push_back(CDNSSeedData("165.22.42.81", "165.22.42.81"));
 //	vSeeds.push_back(CDNSSeedData("149.28.158.213", "149.28.158.213"));
 
